bb52561368294c3487ff9e853f3c8286b6409727
Michele Zanella <zanella.michele@gmail.com>
2015-12-22 15:30:07 +0100

    [controller] Added dataManager as listener in sessionStarter
    Signed-off-by: Michele Zanella <zanella.michele@gmail.com>

--- src/controller/session/sessionStarter.js
+++ src/controller/session/sessionStarter.js
@@ -1,8 +1,11 @@
 exports.startNewConcentrationSession = function() {
     var receiver = require("../headsetReceiver/dataReceiver");
     var attentionTask = require("./attentionTask");
+    var dataManager = require("../dataManager");
 
     receiver.addNewListener(attentionTask.checkPacket);
+    receiver.addNewListener(dataManager.addPacket);
+    
     receiver.startReceiving();
 }
 
