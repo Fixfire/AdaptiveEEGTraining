bc30b0dbccea853c1a0324fb968c6e0403b59552
Michele Zanella <zanella.michele@gmail.com>
2015-12-22 20:22:05 +0100

    [controller] Added Log and DataEntry classes into one JS file
    Signed-off-by: Michele Zanella <zanella.michele@gmail.com>

--- src/controller/dataManager.js
+++ src/controller/dataManager.js
@@ -7,5 +7,49 @@ var log = new Log();
     log.addEntry(packet.getTimestamp(),packet.getAttention(),packet.getRelaxation());
     
     console.log(JSON.stringify(log.getEntries(),null,4));
+
+}
+
+/* CLASS LOG */
+
+//Constructor
+function Log() {
+    
+    this.entries = [];
+    
+}
+
+//Class methods
+Log.prototype.addEntry = function(timestamp,attentionLevel, relaxationLevel) {
+    
+    var data = new DataEntry(timestamp,attentionLevel,relaxationLevel); 
+    //JSONentry = JSON.stringify(data);
+    this.entries.push(data);   
+    
+};
+
+Log.prototype.getEntries = function() {
+    
+    return this.entries;
+    /*this.entries.forEach(function (eachEntry) {
+         console.log(eachEntry);
+    })*/
+    
+};
+
+Log.prototype.getEntry = function() {
+    
+    return this.entries.pop();
+    
+};
+
+/* CLASS DATAENTRY */
+
+//Constructor
+function DataEntry(timestamp, attentionLevel, relaxationLevel) {
+    
+    this.timestamp = timestamp;
+    this.attentionLevel = attentionLevel;
+    this.relaxationLevel = relaxationLevel;
     
 }
