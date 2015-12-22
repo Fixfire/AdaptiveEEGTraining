4f6242eecfb8280bb721956d73e3e069f050e188
ubugu <claudio.ubugu@gmail.com>
2015-12-22 15:00:02 +0100

    [controller] Created a dummy server.
    Created a dummy server that creates random packet in order to test the applcation.
    
    Signed-off-by: ubugu <claudio.ubugu@gmail.com>

---
+++ src/controller/dummyServer.js
@@ -0,0 +1,28 @@
+var events = require('events');
+var packetEmitter = new events.EventEmitter();
+
+exports.newListener = function(listener) {
+    packetEmitter.addListener('newPacket',listener);
+}
+
+console.log("Starting dummy server");
+
+var starter = require('./session/sessionStarter');
+
+starter.startNewConcentrationSession();
+
+var number = 0;
+    
+setInterval(function packetGenerator () {
+    var dataPacket = require("./headsetReceiver/dataPacket");
+
+    var packet = dataPacket.randomPacketGenerator();
+    
+    console.log("emitting packet number " + number);
+    packetEmitter.emit("newPacket",packet);
+    
+    number = number +1;
+
+}, 3000);
+
+console.log("timeout started"); 
