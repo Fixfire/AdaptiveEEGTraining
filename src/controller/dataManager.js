b2090c2025854319da454a63b0809f6e22005484
Michele Zanella <zanella.michele@gmail.com>
2015-12-22 20:22:48 +0100

    [view] Added sample index page to load dataManager JS
    Modified paramter passed to addEntry function to make an example
    
    Signed-off-by: Michele Zanella <zanella.michele@gmail.com>

--- src/controller/dataManager.js
+++ src/controller/dataManager.js
@@ -4,8 +4,8 @@ var log = new Log();
 //exports.addPacket = 
     function addPacket(packet){
     
-    log.addEntry(packet.getTimestamp(),packet.getAttention(),packet.getRelaxation());
-    
+    //log.addEntry(packet.getTimestamp(),packet.getAttention(),packet.getRelaxation());
+    log.addEntry('aaaa:mm:dd::hh:mm:ss',10,10);
     console.log(JSON.stringify(log.getEntries(),null,4));
 
 }
