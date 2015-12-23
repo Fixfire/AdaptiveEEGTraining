c4b8ec8fe20df7c1b68bc80b66b6a8daeb756c46
Alessandro Pappalardo <mail@alessandropappalardo.eu>
2015-12-23 16:11:37 +0100

    [Chrome App] Chrome app basic structure
    Signed-off-by: Alessandro Pappalardo <mail@alessandropappalardo.eu>

---
+++ background.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Alessandro on 23/12/15.
+ */
+chrome.app.runtime.onLaunched.addListener(function() {
+    chrome.app.window.create('window.html', {
+        'outerBounds': {
+            'width': 400,
+            'height': 500
+        }
+    });
+});
