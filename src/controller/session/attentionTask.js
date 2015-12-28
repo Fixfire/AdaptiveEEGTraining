884fe915f3bf9aec7b2f0a219b6cc23e2a47c3fa
Michele Zanella <zanella.michele@gmail.com>
2015-12-28 18:23:59 +0100

    [controller] Created attentionTask class
    Task paramters
    Internal logic of the application when a new packet arrives.
    
    Signed-off-by: Michele Zanella <zanella.michele@gmail.com>

--- src/controller/session/attentionTask.js
+++ src/controller/session/attentionTask.js
@@ -1,5 +1,68 @@
-exports.checkPacket = function(packet) {
-    console.log("I'm attention session and this is the packet ");
-    console.log(packet);
+var level;
+var time;
+var actions;
+var active;
+
+
+function AttentionTask(){
+    this.level = 0;
+    this.time = 0;
+    this.actions = [];
+    this.active = false;
 }
 
+
+
+
+AttentionTask.prototype.checkPacket = function(packet) {
+    console.log("I'm attention session and this is the packet ");
+    console.log(packet);
+    
+    var timeout; 
+    console.log(level);
+    console.log(packet.attention);
+    if(packet.attention >= level && !active){
+        console.log("sei over 9000");
+        timeout = setTimeout(startActions(),500 * time);
+        active = true;
+    }
+    if(packet.attention < level && active){
+        timeout = clearTimeout();
+        active = false;
+    }
+    
+}
+
+
+function startActions(){
+    console.log("Action Started!");
+}
+
+
+
+AttentionTask.prototype.setLevel = function(level){
+    this.level = level;
+}
+
+AttentionTask.prototype.getLevel = function(){
+    return this.level;
+}
+
+AttentionTask.prototype.setTime = function(time){
+    this.time = time;
+}
+
+AttentionTask.prototype.getTime = function(){
+    return this.time;
+}
+
+AttentionTask.prototype.addAction = function(action){
+    this.actions.push(action);
+}
+
+AttentionTask.prototype.getActions = function(){
+    return this.actions;
+}
+
+
+module.exports = AttentionTask;
