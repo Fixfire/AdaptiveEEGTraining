7ed5b3657b07481af3876c3a0b0e6471c6cc86a6
Michele Zanella <zanella.michele@gmail.com>
2015-12-28 18:25:20 +0100

    [controller] Implemented sessionStarter ECA JSON
    Defining ECA JSON format,
    Parsing of the ECA JSON,
    setting of task's parameter,
    adding task to listeners list
    
    
    Signed-off-by: Michele Zanella <zanella.michele@gmail.com>

--- src/controller/session/sessionStarter.js
+++ src/controller/session/sessionStarter.js
@@ -1,15 +1,66 @@
 exports.startNewConcentrationSession = function() {
     var receiver = require("../headsetReceiver/dataReceiver");
-    var attentionTask = require("./attentionTask");
+    var AttentionTask = require("./AttentionTask");
     var dataManager = require("../dataManager");
+    
+    var JSONSession = '[{"main":{"0":{"when":{"event":"attention","level":"50","time":"1"},"do":{"0":{"label":"video","action":"play"},"1":{"label":"hue","action":"on"}}}},"options":{"device":"pc","timeout":"60"}},{"main":{"0":{"when":{"event":"attention","level":"30","time":"2"},"do":{"0":{"label":"video","action":"play"},"1":{"label":"hue","action":"on"}}}},"options":{"device":"pc","timeout":"60"}}]';
+        
+    var taskNumber = 0;
+    
+    var JSONTask = JSON.parse(JSONSession);
+    console.log(JSONTask);
+    
+    for (task in JSONTask){
+    
+        var JSONScene = JSONTask[task].main;
 
-    receiver.addNewListener(attentionTask.checkPacket);
-    receiver.addNewListener(dataManager.addPacket);
+        receiver.addNewListener(dataManager.addPacket);
+
+        for(event in JSONScene){
+
+            var attentionTask = new AttentionTask();
+
+            createTask(JSONScene[event],attentionTask);
+            receiver.addNewListener(attentionTask.checkPacket,taskNumber);
+            
+            taskNumber = taskNumber + 1;
+
+
+        }
+
+    }
     
     receiver.startReceiving();
+    
+    
+    
 }
 
 
 exports.startNewRelaxationSession = function() {
     
+}
+
+function createTask(JSONTask, task){
+    var time = JSONTask.when.time;
+    if(time != ''){
+        task.setTime(time);
+    }
+    console.log("TIME IS: " + task.getTime());
+    
+    var level = JSONTask.when.level;
+     if(level != ''){
+        task.setLevel(level);
+    }
+    console.log("LEVEL IS: " + task.getLevel());
+    
+    var JSONActions = JSONTask.do;
+    
+    for(action in JSONActions){
+        console.log(JSONActions[action]);
+        task.addAction(JSONActions[action]);
+    }
+    
+    console.log("ACTIONS ARE "+task.getActions());
+
 }
