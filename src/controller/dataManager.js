bd64eb8243b3ead419b0aad1430ef47ea78c7736
Michele Zanella <zanella.michele@gmail.com>
2015-12-28 12:04:25 +0100

    [controller] Restored dataManager js with Node
    Signed-off-by: Michele Zanella <zanella.michele@gmail.com>

--- src/controller/dataManager.js
+++ src/controller/dataManager.js
@@ -1,55 +1,11 @@
-//var Log = require('../model/Log');
+var Log = require('../model/Log');
 var log = new Log();
 
-//exports.addPacket = 
+exports.addPacket = 
     function addPacket(packet){
     
-    //log.addEntry(packet.getTimestamp(),packet.getAttention(),packet.getRelaxation());
-    log.addEntry('aaaa:mm:dd::hh:mm:ss',10,10);
+    log.addEntry(packet.getTimestamp(),packet.getAttention(),packet.getRelaxation());
+    //log.addEntry('aaaa:mm:dd::hh:mm:ss',10,10);
     console.log(JSON.stringify(log.getEntries(),null,4));
 
-}
-
-/* CLASS LOG */
-
-//Constructor
-function Log() {
-    
-    this.entries = [];
-    
-}
-
-//Class methods
-Log.prototype.addEntry = function(timestamp,attentionLevel, relaxationLevel) {
-    
-    var data = new DataEntry(timestamp,attentionLevel,relaxationLevel); 
-    //JSONentry = JSON.stringify(data);
-    this.entries.push(data);   
-    
-};
-
-Log.prototype.getEntries = function() {
-    
-    return this.entries;
-    /*this.entries.forEach(function (eachEntry) {
-         console.log(eachEntry);
-    })*/
-    
-};
-
-Log.prototype.getEntry = function() {
-    
-    return this.entries.pop();
-    
-};
-
-/* CLASS DATAENTRY */
-
-//Constructor
-function DataEntry(timestamp, attentionLevel, relaxationLevel) {
-    
-    this.timestamp = timestamp;
-    this.attentionLevel = attentionLevel;
-    this.relaxationLevel = relaxationLevel;
-    
 }
