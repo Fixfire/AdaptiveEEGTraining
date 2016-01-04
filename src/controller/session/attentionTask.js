da5428bb54b07c3a9ce886ca97e58d6998056907
Michele Zanella <zanella.michele@gmail.com>
2016-01-04 16:13:52 +0100

    [controller] Integration of View object
    Require directive for view.js;
    Instantiation of View object;
    Added example JSON to initialize the view object;
    Added view object to event "jsonPacket" to update control panel's graph
    
    
    Signed-off-by: Michele Zanella <zanella.michele@gmail.com>

--- src/controller/session/attentionTask.js
+++ src/controller/session/attentionTask.js
@@ -33,7 +33,13 @@ AttentionTask.prototype.checkPacket = function(packet,object) {
 
 
 function startActions(object){
+    
     var starter = require("./sessionStarter");
+    
+    for (action in object.actions){
+        starter.getView().actions(JSON.stringify(object.actions[action]));
+    }
+    
     starter.removeListener(object);
     console.log("Action Started!");
 }
