011b9b1b7948a2e6ddabd866a01d09ed7373f06c
Alessandro Pappalardo <mail@alessandropappalardo.eu>
2016-01-04 17:11:53 +0100

    [Main] Listen on process event instead of adapter for packets
    Signed-off-by: Alessandro Pappalardo <mail@alessandropappalardo.eu>

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var main = (function() {
     document.addEventListener('DOMContentLoaded', function() {
         var adapter = new Adapter();
         adapter.init();
-        adapter.on("attention", function(data) {
+        process.on("packet", function(data) {
             console.log(data);
         })
     });
