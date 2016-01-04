5b1b485f1c07d627cc43d3e2d4a5cd59a943dd9d
Erica Stella <erica.stella@mail.polimi.it>
2016-01-04 19:35:03 +0100

    [view] charts added and minor refactoring

--- src/view/view.js
+++ src/view/view.js
@@ -10,13 +10,14 @@ function View( JSONInitializer ) {
 
     if(this.environment == "magicRoom"){
         this.lights = settings.lights;
+        setPanel();
     }
     
 }
 
 module.exports = View;
 
-
+//TODO da fare lo stop del video e la fine della sessione
 
 //Distinzione tra azioni
 View.prototype.actions = function( JSONaction ){
@@ -30,7 +31,7 @@ View.prototype.actions = function( JSONaction ){
         var action = settings.action;
         
         if(action == "load"){
-            this.videoOnScreen(settings.path);
+            this.videoOnScreen(JSONaction);
         }
         if(action == "play"){
             this.playVideo();
@@ -42,7 +43,7 @@ View.prototype.actions = function( JSONaction ){
         var action = settings.action;
         
         if(action == "play"){
-            startMusic(settings.path);
+            startMusic(JSONaction);
         }
         if(action == "continue"){
             changeMusicVolume(settings.final_volume, settings.responsive_function);
@@ -53,53 +54,116 @@ View.prototype.actions = function( JSONaction ){
     }
     
     if(action == "light"){
-        this.setLights( settings.color, settings.final_brightness, settings.position);
+        this.setLights(JSONaction);
     }
-}
-
-
-
-//Metodo di startup per le sessioni
-View.prototype.startSession = function(){
-    
-    if(this.environment == "magicRoom"){
-        setPanel();
-    }
-
 }
 
 
 
 //Metodi per inizializzazione e update pannello di controllo
-function setPanel(/*TODO parametri*/) {
+function setPanel() {
     
-    //inizializzo i contenitori per i due grafici
-    $(".main-content").append('<div id="container1" style="width:100%; height:400px;"></div><div id="container2" style="width:100%; height:400px;"></div>');
+    //inizializzo il contenitore per il grafico
+    $(".main-content").append('<div id="container" style="width:100%; height:400px;"></div>');
+    
+    $('#container').highcharts({
+        chart: {
+            type: 'scatter',
+            margin: [50, 50, 60, 80],
+        },
+        title: {
+            text: ''
+        },
+        legend: {
+            enabled: true,
+            floating: true,
+            verticalAlign: 'bottom',
+            layout: 'vertical', 
+            align: 'center',
+            y: 25
+        },
+        xAxis: {
+            gridLineWidth: 1,
+            minPadding: 0.2,
+            maxPadding: 0.2,
+            min: 0,
+            floor: 0,
+            tickInterval: 2
+        },
+        yAxis: {
+            gridLineWidth: 1,
+            floor: 0,
+            min: 0,
+            ceiling: 100,
+            max: 100,
+            title: {
+                text: 'Valore'
+            },
+            minPadding: 0.2,
+            maxPadding: 0.2,
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        exporting: {
+            enabled: false
+        },
+        plotOptions: {
+            area: {
+                pointStart: 0,
+                marker: {
+                    enabled: true
+                },
+            },
+            series: {
+                lineWidth: 1
+            }
+        },
+        series: [{
+            name: 'Attenzione',
+            data: [[20, 20], [30, 30]],
+            color: '#ffbf00'
+        },
+        {
+            name: 'Rilassamento',
+            data: [[25, 56], [56, 37]],
+            color: '#00bfff'
+        }]
+    });
+    
 }
 
 View.prototype.updateGraph = function( packet ) {
-    if($(/*TODO id del contenitore del grafico*/).length){
-        //TODO update graph
+    
+    //controllo che il container esista e quindi ci sia il pannello 
+    if($("#container").length){
+              
+        var graph = $("#container").highcharts();
+        
+        graph.series[0].addPoint([packet.timestamp, packet.attention], true);
+        graph.series[1].addPoint([packet.timestamp, packet.meditation], true);
     }
 }
-
 
 
 
 //Metodi per gestione dei video
-View.prototype.videoOnScreen = function( video ){
+View.prototype.videoOnScreen = function( videoJSON ){
     
     if(this.environment == "magicRoom"){
-        videoOnScreenWithProjector( video );
-        updateAttentionPanel(/*parametri*/);
+        videoOnScreenWithProjector( videoJSON );
     }
     else {
-        videoOnScreenOnBrowser( video );
+        
+        var video = JSON.parse(videoJSON);
+        videoOnScreenOnBrowser( video.path );
     }
 }
 
-function videoOnScreenWithProjector( video ) {
-    //TODO fare il JSON con comando per la stanza di fare il load del video
+function videoOnScreenWithProjector( videoJSON ) {
+    //TODO 
 }
 
 function videoOnScreenOnBrowser( video ) {
@@ -134,7 +198,7 @@ function playVideoOnBrowser(){
 
 
 //Metodi per la gestione della stanza
-function startMusic( music ) {
+function startMusic( musicJSON ) {
     //TODO chiamare SSex
 }
 
@@ -146,7 +210,7 @@ function stopMusic(){
     //TODO chiamare SSex 
 }
 
-View.prototype.setLights = function( color, intensity, position ){
+View.prototype.setLights = function( lightsJSON ){
     //TODO chiamare SSex
 }
 
