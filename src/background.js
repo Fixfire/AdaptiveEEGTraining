163b92203db69df9b323893ee067bc918bd5024a
Michele Zanella <zanella.michele@gmail.com>
2016-01-07 12:55:23 +0100

    [browserify] Eliminated browserify dependencies files
    Eliminated Makefile
    
    Signed-off-by: Michele Zanella <zanella.michele@gmail.com>

--- src/background.js
+++
@@ -1,8 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-    chrome.app.window.create('./view/index.html', {
-        'outerBounds': {
-            'width': 1280,
-            'height': 1024
-        }
-    });
-});
