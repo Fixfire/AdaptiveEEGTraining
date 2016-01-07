2e108af450ed391edf5f6952d2f32b08cd76c514
Michele Zanella <zanella.michele@gmail.com>
2016-01-07 14:37:19 +0100

    [main] Added require for DummyServer and starter
    Signed-off-by: Michele Zanella <zanella.michele@gmail.com>

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 var Adapter = require("./adapter.js");
+var Server = require("./src/controller/dummyServer.js");
 
 var main = (function() {
 
     document.addEventListener('DOMContentLoaded', function() {
+        
         var adapter = new Adapter();
         adapter.on("packet", function(data) {
             console.log(JSON.stringify(data));
         })
         adapter.init();
+        Server.start();
     });
 
 })();
