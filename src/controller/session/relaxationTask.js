4b973f2f3f2bec774d78fcf0dad57aefb9117dd8
Michele Zanella <zanella.michele@gmail.com>
2016-01-08 11:35:36 +0100

    [controller] Implemented control on relaxation task
    Signed-off-by: Michele Zanella <zanella.michele@gmail.com>

--- src/controller/session/relaxationTask.js
+++ src/controller/session/relaxationTask.js
@@ -3,44 +3,59 @@ var time;
 var actions;
 var active;
 var timeout; 
+var condition;
 
 function RelaxationTask(){
     this.level = 0;
     this.time = 0;
     this.actions = [];
     this.active = false;
+    this.condition = "";
 }
 
 
 RelaxationTask.prototype.checkPacket = function(packet,object) {
-    console.log("I'm attention session and this is the packet ");
+    console.log("I'm relaxation session and this is the packet ");
     console.log(packet);
 
-    //Logging in cosole for checking
+     //Logging in cosole for checking
     console.log("rule level : " + object.level);
     console.log("rule time : " + object.time);
-
-    if(packet.meditation >= object.level && !object.active){
-        object.timeout = setTimeout(function() {startActions(object);},1000 * object.time);
-        object.active = true;
-    }
-    if(packet.meditation < object.level && object.active){
-        object.timeout = clearTimeout();
-        object.active = false;
-    }
+    if (object.condition == "above") {
+        
+        if(packet.relaxation >= object.level && !object.active){
+            object.timeout = setTimeout(function() {startActions(packet,object);}, 1000 * object.time);
+            object.active = true;
+        }
+        if(packet.relaxation < object.level && object.active){
+            object.timeout = clearTimeout();
+            object.active = false;
+        }
+    } else if (object.condition == "below") {
+        if(packet.relaxation <= object.level && !object.active){
+            object.timeout = setTimeout(function() {startActions(packet,object);}, 1000 * object.time);
+            object.active = true;
+        }
+        if(packet.relaxation > object.level && object.active){
+            object.timeout = clearTimeout();
+            object.active = false;
+        }
+    } else {
+        startActions(packet,object);
+    } 
     
 }
 
 
 function startActions(){
-    var starter = require("./sessionStarter");
+     var starter = require("./sessionStarter");
+    console.log("Action Started!");
     
     for (action in object.actions){
-        starter.getView().actions(JSON.stringify(object.actions[action]));
+        starter.getView().actions(packet,JSON.stringify(object.actions[action]));
     }
 
-    starter.removeListener(object);
-    console.log("Action Started!");
+    //starter.removeListener(object);
 }
 
 
