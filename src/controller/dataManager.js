c04471a519c3d41304532b38a34313924b508170
Michele Zanella <zanella.michele@gmail.com>
2016-01-10 20:01:25 +0100

    [model] Fixed wrong attribute name getter function
    Signed-off-by: Michele Zanella <zanella.michele@gmail.com>

--- src/controller/dataManager.js
+++ src/controller/dataManager.js
@@ -4,8 +4,11 @@ var log = new Log();
 exports.addPacket = 
     function addPacket(packet){
     
-    log.addEntry(packet.getTimestamp(),packet.getAttention(),packet.getRelaxation());
+    log.addEntry(packet.getTimestamp(),packet.getAttention(),packet.getMeditation());
     //log.addEntry('aaaa:mm:dd::hh:mm:ss',10,10);
-   // console.log(JSON.stringify(log.getEntries(),null,4));
+   //console.log(JSON.stringify(log.getEntries(),null,4));
+    
+
+}   
 
 }   
