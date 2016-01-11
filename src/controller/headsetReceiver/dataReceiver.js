653074d51cb488ecc92964e06d44c1b5045bf3e6
Michele Zanella <zanella.michele@gmail.com>
2016-01-11 16:20:39 +0100

    [controller] Added management to clear timeout of task of finished session
    Signed-off-by: Michele Zanella <zanella.michele@gmail.com>

--- src/controller/headsetReceiver/dataReceiver.js
+++ src/controller/headsetReceiver/dataReceiver.js
@@ -6,6 +6,11 @@ var adapter;
 
 var dataManager = require('../dataManager');
 
+exports.addNewSessionListener = function(element){
+    console.log("added new session for the current task");
+    packetEmitter.addListener('endSession', element);
+}
+
 exports.addNewTaskListener = function(element) {
     console.log("added new rule for the current task");
     listeners.push(element);
@@ -14,6 +19,10 @@ exports.addNewTaskListener = function(element) {
 
 exports.removeTaskListener = function(listener) {
     packetEmitter.removeAllListeners(listeners.indexOf(listener) + 1 );
+    listeners.splice(listeners.indexOf(listener),1);
+    if (listeners.length == 0){
+        packetEmitter.emit('endSession');
+    }
 }
 
 exports.addNewListener = function(listener) {
@@ -29,9 +38,11 @@ exports.startReceiving = function() {
 exports.stopTasks = function(){
     for (task in listeners){
         var target = parseInt(task) + 1;
+        clearTimeout(listeners[task].getTimeout());
         packetEmitter.removeListener(target,listeners[task].checkPacket);
     }
-    dataManager.save();
+    packetEmitter.emit('endSession');
+    //dataManager.save();
 }
 
 exports.setAdapter = function(adapt) {
