983fde4934723ea78ff47b5437195da098fb2769
Michele Zanella <zanella.michele@gmail.com>
2016-01-14 11:56:44 +0100

    [controller] Changed packet variables accessing call
    To meet transparency with bt packets
    
    Signed-off-by: Michele Zanella <zanella.michele@gmail.com>

--- src/controller/dataManager.js
+++ src/controller/dataManager.js
@@ -4,7 +4,7 @@ var log = new Log();
 exports.addPacket = 
     function addPacket(packet){
     
-    log.addEntry(packet.getTimestamp(),packet.getAttention(),packet.getMeditation());
+    log.addEntry(packet.timestamp,packet.attention,packet.meditation);
     //log.addEntry('aaaa:mm:dd::hh:mm:ss',10,10);
    //console.log(JSON.stringify(log.getEntries(),null,4));
     
