1d7933f292487f0e881bff3827011fe3b3b626d1
Alessandro Pappalardo <mail@alessandropappalardo.eu>
2016-01-16 21:06:07 +0100

    [Background] Add serial disconnection on app closing
    Signed-off-by: Alessandro Pappalardo <mail@alessandropappalardo.eu>

--- background.js
+++ background.js
@@ -11,3 +11,28 @@ chrome.app.runtime.onLaunched.addListener(function() {
     });
     
 });
+
+chrome.runtime.onSuspend.addListener(function() {
+  chrome.serial.getConnections(function(connectionIdList) {
+    var connectionId;
+    for (connectionId in connectionIdList) {
+      chrome.serial.setPaused(connectionId, true, function() {
+        chrome.serial.flush(connectionId, function(resultFlush) {
+          console.log("Flushed serial port");
+          if (resultFlush) {
+            chrome.serial.disconnect(connectionId, function(resultDisconnect) {
+  			  if (resultDisconnect) {
+    			console.log("Disconnected from the serial port");
+  			  } else {
+    		  console.log("Disconnect failed");
+    		  }
+  			});
+  			} else {
+    		console.log("Flush failed");
+  			}
+        });
+      });
+ 
+    }
+  });
+});
