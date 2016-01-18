604e43ccd784a2d83b09a7278c5aa65d59ed8bfb
ubugu <claudio.ubugu@gmail.com>
2016-01-19 00:44:31 +0100

    [extension] Removed starter extension.
    Signed-off-by: ubugu <claudio.ubugu@gmail.com>

--- EEGextensionStarter/starter.js
+++
@@ -1,9 +0,0 @@
-
-var queryString = (window.location.href).substr((window.location.href).indexOf('?') + 1); 
-var value = (queryString.split('='))[1];
-value = decodeURIComponent(value);
-
-
-
-chrome.storage.local.set({'JSONsession': value});
-chrome.management.launchApp("hhccckalaoabhoffpkjpifaigiinhblo");
