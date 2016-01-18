3b5cec8c53a414a907ab2bbf19221c92c84da2ea
ubugu <claudio.ubugu@gmail.com>
2016-01-19 00:51:42 +0100

    [controller] Moved panel.js that handle the panel.html script into main.js
    Signed-off-by: ubugu <claudio.ubugu@gmail.com>

--- main.js
+++ main.js
@@ -11,6 +11,10 @@ var main = (function() {
         var session = getUrlVars()["JSONSession"];
         //TODO var json = require("./" + session + ".json");
         var json = require("./JSON.json");
+var port ;
+var dummyBool;
+
+document.addEventListener('DOMContentLoaded', function() {
         
         if (dummy == "true") {
             adapter = new Dummy();
@@ -25,16 +29,80 @@ var main = (function() {
             dataReceiver.setAdapter(adapter);
             adapter.init();
         } 
+    
+    $('#portListButton').on('click', function setPortList(callback) {
+        var list = document.getElementById('portListDropdown');
+        chrome.serial.getDevices(function(portList) {
+            console.log(portList);
+            for (var entry = 0; entry < portList.length; entry++) {
+                var path = String(portList[entry].path);
+                var li = document.createElement("li");
+                var link = document.createElement("a");             
+                var text = document.createTextNode(path);
+                link.appendChild(text);
+                link.href = "#";
+                li.appendChild(link);
+                list.appendChild(li);
+            }
+        });
     });
-})();
+ 
+/* WIP copy selected to entry text field
+    $('.portListEntry').on('click', function setPortList(callback) {
+        console.log("clicked on " + this.text());
+        var port = document.getElementById('port');
+        var path = this.text();
+        port.val(path);
+        
+    });
+    */
+});
 
 function getUrlVars() {
     var vars = {};
     var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
     vars[key] = value;
+
+
+//Check input and start the application.
+function startApplication() {
+    
+    //chechk if JSON was inserted or loaded.
+    if (json == undefined){
+        var opt = {        
+            type: "basic",
+            title: "ALERT",
+            message: "JSON is not inserted\wait for json to reload",
+            iconUrl: "../../alert.jpg"
+        }
+
+        chrome.notifications.create("string notificationId",opt);
+        return;
+    }
+    
+    port = document.getElementById("port").value;
+    dummyBool = document.getElementById("dummyBool").checked;
+    
+    //Check if port is inserted with dummy disabled.
+    if(dummyBool == false) {
+        if (port == ''){
+            var opt = {        
+                type: "basic",
+                title: "ALERT",
+                message: "port is not defined for not dummy applcation!",
+                iconUrl: "../../alert.jpg"
+            }
+
+            chrome.notifications.create("string notificationId",opt);
+            return;
+        }      
+    }
     });
     return vars;
+  
 }
+}
+
 
 
 
