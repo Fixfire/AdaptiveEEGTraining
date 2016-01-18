2cf5c51718d5fb42a75e306bc4d3558349c53f7b
Michele Zanella <zanella.michele@gmail.com>
2016-01-18 13:10:27 +0100

    [controller] Added abilia db session field
    WIP: Added implementation of abilia db call to send logs
    
    Signed-off-by: Michele Zanella <zanella.michele@gmail.com>

--- src/controller/dataManager.js
+++ src/controller/dataManager.js
@@ -3,6 +3,17 @@ var ActionLog = require('../model/ActionLog');
 var dataLog = new DataLog();
 var actionLog = new ActionLog();
 
+var idTherapeuticCenter;
+var idActivity;
+var idChild;
+var idTherapist;
+var idParent;
+var notes = "";
+var rating = 5;
+var dateStart;
+var dateEnd;
+var outcome = "positive";
+
 /* Add packet to log when a new packet arrives */
 exports.addPacket = 
     function addPacket(packet){
@@ -24,5 +35,45 @@ exports.addAction =
 exports.save = function(){
     console.log(JSON.parse(dataLog.createJSON()));
     console.log(JSON.parse(actionLog.createJSON()));
+    console.log("Date start: " + dateStart);
+    console.log("Date end: " + dateEnd);
+    
     //TODO: Call Abilia DB
+    /*
+    var dbInitializer = '{"idTherapeuticCenter": "' + idTherapeuticCenter + '","idActivity": "'+ idActivity + '","idChild": "' + idChild + '","idTherapist": "' + idTherapist + '","idParent": "' + idParent + '","notes": "' + notes + '","rating": "' + rating + '","fieldList": {"timestamp":"DATETIME","attention": "INT(10)","relaxation": "INT(3)",“label”: “VARCHAR(32)”,"action": "VARCHAR(32)",“intensity”: “FLOAT”,“color”:”VARCHAR(32)”,“path”:”VARCHAR(100)”},"dateStart": "' + dateStart + '","dateEnd": "' + dateEnd + '","outcome": "' + outcome + '"}';
+    $.ajax({
+        type: "POST",
+        url: "url",             <---- INSERIRE URL SERVER --->
+        data: dbInitializer,
+        dataType: "json",
+    }).done(function() {
+    
+        var dataLogJSON = dataLog.createJSON();
+    
+        $.ajax({
+            type: "POST",
+            url: "url",             <---- INSERIRE URL SERVER --->
+            data: dataLogJSON,
+            dataType: "json"
+        });
+        
+        var actionLogJSON = actionLog.createJSON();
+
+        $.ajax({
+            type: "POST",
+            url: "url",             <---- INSERIRE URL SERVER --->
+            data: actionLogJSON,
+            dataType: "json"
+        });
+    });
+    */
+
+}
+
+exports.setDateStart = function(date){
+    dateStart = date;
+}
+
+exports.setDateEnd = function(date){
+    dateEnd = date;
 }
