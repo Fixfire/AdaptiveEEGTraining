f37861a7a08965b5ad88cdbecf56ec38066aed6c
ubugu <claudio.ubugu@gmail.com>
2016-01-19 11:44:39 +0100

    [controller] Rewriting of dummy in order to make it work as adpater.js
    Signed-off-by: ubugu <claudio.ubugu@gmail.com>

--- Dummy.js
+++ Dummy.js
@@ -1,34 +1,29 @@
 //Section for dummy server
-var events = require('events');
-var packetEmitter = new events.EventEmitter();
 var number = 0;
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
+
+util.inherits(Dummy, EventEmitter);
+module.exports = Dummy
 
 function Dummy() {
     
 }
 
-
-Dummy.prototype.on = function(event,listener) {
-    console.log("ADDING NEW LISTENER");
-    packetEmitter.addListener(event,listener);
-}
-
-Dummy.prototype.startDummy = function(){
+//Init function for starting sending random packet every 1 secons
+Dummy.prototype.init = function() {
     console.log("Starting dummy server");
-    
-
+    var object = this;
     setInterval(function packetGenerator () {
         var dataPacket = require("./src/controller/headsetReceiver/dataPacket");
 
         var packet = dataPacket.randomPacketGenerator();
 
         console.log("emitting packet number " + number);
-        packetEmitter.emit("packet",packet);
+        object.emit("packet",packet);
 
         number = number +1;
 
     }, 1000);
-
 }
 
-module.exports = Dummy
